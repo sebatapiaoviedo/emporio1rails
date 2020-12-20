d67fb4b53fe724d3af254861bb7a05c5047d26cc
sebatapiaoviedo <sebatapiaoviedo@gmail.com>
2020-12-20 20:45:40 -0300

    primer commit

---
+++ config/routes.rb
@@ -0,0 +1,6 @@
+Rails.application.routes.draw do
+  get 'pages/index'
+  get 'pages/about'
+  root 'pages#index'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
